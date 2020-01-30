506b6b122975ab804e70eeef6ef312d9cccdda99
Alina <alina.varkki.2018@uni.strath.ac.uk>
2020-01-30 12:17:51 +0000

    first stage

---
+++ cmake-build-debug/CMakeFiles/CSHELL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CSHELL.dir/main.c.obj"
+  "CSHELL.exe"
+  "CSHELL.exe.manifest"
+  "CSHELL.pdb"
+  "libCSHELL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CSHELL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CSHELL.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CSHELL.
+# This may be replaced when dependencies are built.
