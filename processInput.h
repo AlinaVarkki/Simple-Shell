34e2a1d44faee573ac9f6671e434d7a3044e61d2
jjb18169 <alina.varkki.2018@uni.strath.uk>
2020-02-18 11:22:15 +0000

    stage 3 done(need to get rid of al the tons of error messages) no processinput yet

--- processInput.h
+++ processInput.h
@@ -40,31 +40,6 @@ char** parsingTheLine(char* input) {
     return tokens;
 }
 
-
-/**
- * gets rid of the 0s/empty spaces that follow the string
- * @param line/input to be trimmed
- * @return trimmed line
- */
-
-char* trimString(char* line) {
-    int len = stringLength(line);
-    line[len-1] = '\0';
-    return line;
-}
-
-/**
- * @param line
- * @return length of the line
- */
-int stringLength(const char* line) {
-    int i = 0;
-    while (line[i] != '\0') {
-        i++;
-    }
-    return i;
-}
-
 /**
  * mehod prints the current path
  */
